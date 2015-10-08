58d9124061c1a415df79923aa38566623712fe82
Benjie Gillam <benjie@jemjie.com>
2015-10-08 09:27:52 +0100

    Add tests, strict equality.

--- test/test.js
+++ test/test.js
@@ -20,14 +20,14 @@ describe('Immutable backbone', function() {
       var model = new Backbone.Model({id: 1, foo: 'bar'});
       var oldAttributes = model.attributes;
       model.set('foo', 'baz');
-      expect(model.attributes).to.eql(oldAttributes);
+      expect(model.attributes).to.equal(oldAttributes);
     });
 
     it("replaces attributes on changing a property", function() {
       var model = new ImmutableModel({id: 1, foo: 'bar'});
       var oldAttributes = model.attributes;
       model.set('foo', 'baz');
-      expect(model.attributes).not.to.eql(oldAttributes);
+      expect(model.attributes).not.to.equal(oldAttributes);
     });
 
   });
@@ -38,14 +38,36 @@ describe('Immutable backbone', function() {
       var collection = new Backbone.Collection([{id: 1, foo: 'bar'}]);
       var oldModels = collection.models;
       collection.add({id: 2, foo: 'baz'});
-      expect(collection.models).to.eql(oldModels);
+      expect(collection.models).to.equal(oldModels);
     });
 
     it("replaces models on add", function() {
       var collection = new ImmutableCollection([{id: 1, foo: 'bar'}]);
       var oldModels = collection.models;
       collection.add({id: 2, foo: 'baz'});
-      expect(collection.models).not.to.eql(oldModels);
+      expect(collection.models).not.to.equal(oldModels);
+    });
+
+    it("replaces models on remove", function() {
+      var collection = new ImmutableCollection([{id: 1, foo: 'bar'}, {id: 2, foo: 'baz'}]);
+      var oldModels = collection.models;
+      collection.remove(2);
+      expect(collection.models).not.to.equal(oldModels);
+    });
+
+    it("replaces models on reset", function() {
+      var collection = new ImmutableCollection([{id: 1, foo: 'bar'}, {id: 2, foo: 'baz'}]);
+      var oldModels = collection.models;
+      collection.reset([]);
+      expect(collection.models).not.to.equal(oldModels);
+    });
+
+    it("replaces models on sort", function() {
+      var collection = new ImmutableCollection([{id: 1, foo: 'bar'}, {id: 2, foo: 'baz'}]);
+      var oldModels = collection.models;
+      collection.comparator = 'foo';
+      collection.sort();
+      expect(collection.models).not.to.equal(oldModels);
     });
 
   });
